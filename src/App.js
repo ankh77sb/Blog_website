66014bb0789f7bb6360796afd7e38b65e13d6599
Shreya Deep <62745351+ayerhs7@users.noreply.github.com>
2020-12-07 13:28:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import { Container } from 'reactstrap';
 import Navbar from './Components/navbar';
 import Home from './Components/Home';
 import Dashboard from './Components/dashboard';
-
+import Editprofile from './Components/editprofile';
+import Bookmark from './Components/bookmark';
 
 
 class App extends Component {
@@ -13,9 +14,11 @@ class App extends Component {
     return (
       <BrowserRouter>
     <div className="App">
-        <Navbar />
-            <Route exact path ='/' component={Home} />
-            <Route path ='/dashboard' component={Dashboard} /> 
+        <Navbar/>
+            <Route exact path ='/' component={Home}/>
+            <Route path ='/dashboard' component={Dashboard}/> 
+            <Route path='/editprofile' component={Editprofile}/>
+            <Route path='/bookmark' component={Bookmark}/>
     </div>
     </BrowserRouter>
     );
